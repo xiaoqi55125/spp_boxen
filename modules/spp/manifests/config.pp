90e89e402edee357570c18887a51f95b8d9bd3fa
Matthew Riddle <mriddle89@gmail.com>
2013-03-19 09:01:53 +1100

    Added iTerm2 preferences

--- modules/spp/manifests/config.pp
+++ modules/spp/manifests/config.pp
@@ -1,3 +1,8 @@
 class spp::config {
 
+  file { "iTerm2 Preferences":
+      path => "/Users/${::luser}/Library/Preferences/com.googlecode.iterm2.plist",
+      source  => "puppet:///modules/spp/com.googlecode.iterm2.plist",
+  }
+  
 }
