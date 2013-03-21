9a00938b2f3632b779626a239e61bef200a73333
Matthew Riddle <mriddle89@gmail.com>
2013-03-21 21:21:40 +1100

    Added mriddle dotfiles

--- modules/people/manifests/mriddle.pp
+++ modules/people/manifests/mriddle.pp
@@ -1,3 +1,31 @@
 class people::mriddle {
   include minecraft
+
+  $my_home = "/Users/${::luser}"
+  $my_sourcedir = $home = "${my_home}/${::srcdir}"
+
+  repository { "${my_sourcedir}/dotfiles":
+    source => 'mriddle/dotfiles',
+  }
+
+  define add_dotfile ($dotfile = $title) {
+    file { "${my_home}/.${dotfile}":
+      ensure  => link,
+      mode    => '0644',
+      target  => "${my_sourcedir}/dotfiles/.${dotfile}",
+      require => Repository["${my_sourcedir}/dotfiles"],
+    }
+  }
+  
+  add_dotfile {'ackrc': }
+  add_dotfile {'bash_aliases': }
+  add_dotfile {'bash_profile': }
+  add_dotfile {'bashrc': }
+
+  file { "${home}/.bash_profile":
+    ensure  => present,
+    content => '[[ -f /opt/boxen/env.sh ]] && . /opt/boxen/env.sh'
+  }
+
+
 }
