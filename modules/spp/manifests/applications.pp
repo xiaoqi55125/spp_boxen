8eea1b7039b40587193701654cd235aac0a9064c
Matthew Riddle <mriddle89@gmail.com>
2013-03-21 21:24:56 +1100

    DBVisualizer is a pkgdmg not appdmg

--- modules/spp/manifests/applications.pp
+++ modules/spp/manifests/applications.pp
@@ -9,11 +9,11 @@ class spp::applications {
   include shiftit
   include xquartz
 
-  # package { "DbVisualizer":
-  #   ensure => "present",
-  #   provider => 'appdmg', 
-  #   source => 'http://www.dbvis.com/form/registerDownload.jsp?prodid=73&product=DbVisualizer&version=9.0.5&version_type=current&target_os=mac&with_jre=false&filename=dbvis_macos_9_0_5.dmg&type=Mac+OS+X+(installer)&installer=%2fproduct_download%2fdbvis-9.0.5%2fmedia%2fdbvis_macos_9_0_5.dmg'
-  # }
+  package { "DbVisualizer":
+    ensure => "present",
+    provider => 'pkgdmg', 
+    source => 'http://www.dbvis.com/form/registerDownload.jsp?prodid=73&product=DbVisualizer&version=9.0.5&version_type=current&target_os=mac&with_jre=false&filename=dbvis_macos_9_0_5.dmg&type=Mac+OS+X+(installer)&installer=%2fproduct_download%2fdbvis-9.0.5%2fmedia%2fdbvis_macos_9_0_5.dmg'
+  }
 
   package { "Quicksilver":
     ensure => "present",
