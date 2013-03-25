35d35f2d18d03255901a45b71e6bba819679f851
Matthew Riddle <mriddle89@gmail.com>
2013-03-26 08:46:28 +1100

    Updated pow module

--- modules/pow/manifests/init.pp
+++ modules/pow/manifests/init.pp
@@ -23,42 +23,46 @@ class pow {
               exec { "grep -vFx '${line}' '${file}' | tee '${file}' > /dev/null 2>&1":
                 onlyif => "grep -qFx '${line}' '${file}'"
               }
-
-              # Use this resource instead if your platform's grep doesn't support -vFx;
-              # note that this command has been known to have problems with lines containing quotes.
-              # exec { "/usr/bin/perl -ni -e 'print unless /^\\Q${line}\\E\$/' '${file}'":
-              #     onlyif => "/bin/grep -qFx '${line}' '${file}'"
-              # }
           }
       }
   }
 
-  # launch at login
+  # Add dev resolver
+  file { "/etc/resolver/dev":
+    content => "nameserver 127.0.0.1\nport 20560",
+    group   => 'wheel',
+    owner   => 'root',
+  }->
+
+  # Setup firewall
+  file { "/Library/LaunchDaemons/cx.pow.firewall.plist":
+    source  => "puppet:///modules/pow/cx.pow.firewall.plist",
+    group   => 'wheel',
+    owner   => 'root',
+  }->
+  exec { "unload pow firewall":
+    command => "launchctl unload '/Library/LaunchDaemons/cx.pow.firewall.plist' || true",
+  }->
+  exec { "load pow firewall":
+    command => "launchctl load -w '/Library/LaunchDaemons/cx.pow.firewall.plist'",
+    user => 'root',
+  }->
+
+  # # launch at login
   file { "${$home}/Library/LaunchAgents/cx.pow.powd.plist":
     source  => "puppet:///modules/pow/cx.pow.powd.plist",
     require => Package["pow"],
   }->
   exec { "unload pow":
-    command => "launchctl unload '${home}/Library/LaunchAgents/cx.pow.powd.plist' 2>/dev/null || true",
+    command => "launchctl unload '${home}/Library/LaunchAgents/cx.pow.powd.plist' || true",
   }->
   exec { "load pow":
-    command => "launchctl load -Fw '${home}/Library/LaunchAgents/cx.pow.powd.plist' 2>/dev/null",
+    command => "launchctl load -w '${home}/Library/LaunchAgents/cx.pow.powd.plist'",
   }->
-  # Setup firewall
-  file { "/Library/LaunchDaemons/cx.pow.firewall.plist":
-    source  => "puppet:///modules/pow/cx.pow.firewall.plist",
-  }->
-  # line { "setup local nameserver":
-  #   file => "/etc/resolver/${::luser}",
-  #   line => "port 20560",
-  # }->
-  file { "/etc/resolver/local":
-    content => "nameserver 127.0.0.1\nport 20560",
-  }
-  exec { "unload pow firewall":
-    command => "launchctl unload '/Library/LaunchDaemons/cx.pow.firewall.plist' 2>/dev/null || true",
-  }->
-  exec { "load pow firewall":
-    command => "launchctl load -Fw '/Library/LaunchDaemons/cx.pow.firewall.plist' 2>/dev/null",
-  }
+  # Still need to run curl get.pow.cx/ | sh
+  # Cause the server works but hitting atlas.dev times out.. I am missing a step in the install process
+  # Why I do it like this? Because running the module via github redbubble/pow or lonelyplanet/pow doesn't work
+  # Getting puppet to exec {curl get.pow.cx/ | sh} also does not work. They fail for the same reason. Trying to
+  # interact with the pow install. Running pow --install-local or any other --command hangs!
+
 }
