25ea014d54f5400bc5c6c3f5d4a540ae231967ea
Matthew Riddle <mriddle89@gmail.com>
2013-03-26 11:01:18 +1100

    Added sppdev dotfiles

---
+++ modules/people/manifests/sppdev.pp
@@ -0,0 +1,19 @@
+class people::sppdev {
+
+  repository { "${::boxen_srcdir}/pairing_station":
+    source => 'lonelyplanet/pairing_station',
+  }
+
+  define add_dotfile ($dotfile = $title) {
+    file { "/Users/${::luser}/.${dotfile}":
+      ensure  => link,
+      mode    => '0644',
+      target  => "${::boxen_srcdir}/pairing_station/${dotfile}",
+      require => Repository["${::boxen_srcdir}/pairing_station"],
+    }
+  }
+  
+  add_dotfile {'bashrc': }
+  add_dotfile {'bash_profile': }
+
+}
