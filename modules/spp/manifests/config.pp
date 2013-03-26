81f053f2cbb89344be43bdd235123089f627f337
Matthew Riddle <mriddle89@gmail.com>
2013-03-26 16:23:21 +1100

    Disable the stupid lion Gatekeeper

--- modules/spp/manifests/config.pp
+++ modules/spp/manifests/config.pp
@@ -7,8 +7,14 @@ class spp::config {
       source  => "puppet:///modules/spp/com.googlecode.iterm2.plist",
   }
 
-  exec {"git config --global color.ui true":
-    require => Package['boxen/brews/git']
+  exec {"Add colour to git output":
+    command => "git config --global color.ui true",
+    require => Package['boxen/brews/git'],
+  }
+
+  exec {"Disable Gatekeeper":
+    command => "spctl --master-disable",
+    user => "root", # Needs to be sudo 'cause it returns successfully and does nothing without it
   }
 
 }
