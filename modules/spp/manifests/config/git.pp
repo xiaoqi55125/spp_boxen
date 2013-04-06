335445437ccbf432180248929472b759fe3aa243
Matt Riddle <matthew.riddle@lonelyplanet.com.au>
2013-04-06 22:26:48 +1100

    Nicer way to add to git config, means other people can add stuff in their manifests without conflicting

--- modules/spp/manifests/config/git.pp
+++ modules/spp/manifests/config/git.pp
@@ -5,42 +5,32 @@ class config::git {
     provider => "gem",
   }
 
-  file { "/Users/${::luser}/.gitconfig":
-    content => "
-[hub]
-  protocol = https
-
-[credential]
-  helper = /opt/boxen/bin/boxen-git-credential
-
-[core]
-  excludesfile = /opt/boxen/config/git/gitignore
-  editor = subl -w
-
-[color]
-  diff = auto
-  status = auto
-  branch = auto
-  ui = true
-
-[push]
-  default = simple
-
-[git-pair]
-  authors = Jonathan Ricketson <jonathanr@lonelyplanet.com.au>
-  authors = Duncan Warnock <duncan.warnock@lonelyplanet.com.au>
-  authors = David Hodges <david.hodges@lonelyplanet.com.au>
-  authors = Matt Riddle <matthew.riddle@lonelyplanet.com.au>
-  authors = Adel Smee <adel.smee@lonelyplanet.com.au>
-  authors = James Holmes <james.holmes@lonelyplanet.com.au>
-  authors = Ben Galvin <ben.galvin@lonelyplanet.com.au>
-  authors = Tim Moore <tim.moore@lonelyplanet.com.au>
-[user]
-  name =
-  email =
-    ",
-    require => Package['boxen/brews/git'],
+  git::config::global{ 'color.ui':
+    value => 'auto',
   }
 
+  git::config::global{ 'push.default':
+    value => 'simple',
+  }
+
+  git::config::global{ 'core.editor':
+    value => 'subl -w',
+  }
+
+  define add_git_pair ($user_details = $title) {
+    exec {"Add ${user_details} git pair":
+      command => "git config --global --add git-pair.authors \"${user_details}\"",
+      unless => "grep -c \"${user_details}\" /Users/${::luser}/.gitconfig"
+    }
+  }
+
+  add_git_pair{"Jonathan Ricketson <jonathanr@lonelyplanet.com.au>":}
+  add_git_pair{"Duncan Warnock <duncan.warnock@lonelyplanet.com.au>":}
+  add_git_pair{"David Hodges <david.hodges@lonelyplanet.com.au>":}
+  add_git_pair{"Matt Riddle <matthew.riddle@lonelyplanet.com.au>":}
+  add_git_pair{"Adel Smee <adel.smee@lonelyplanet.com.au>":}
+  add_git_pair{"James Holmes <james.holmes@lonelyplanet.com.au>":}
+  add_git_pair{"Ben Galvin <ben.galvin@lonelyplanet.com.au>":}
+  add_git_pair{"Tim Moore <tim.moore@lonelyplanet.com.au>":}
 
 }
