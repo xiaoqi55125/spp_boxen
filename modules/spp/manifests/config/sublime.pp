e987da0a23874492120e1449940cc2ae83bd8148
Matt Riddle <matthew.riddle@lonelyplanet.com.au>
2013-04-11 07:33:03 +1000

    Added lonelyplanet test runner plugin to sublime

--- modules/spp/manifests/config/sublime.pp
+++ modules/spp/manifests/config/sublime.pp
@@ -71,7 +71,8 @@ class config::sublime {
     "drewda/cucumber-sublime2-bundle",
     "natew/ExpandSelectionByParagraph",
     "kemayo/sublime-text-2-git",
-    "dzhibas/SublimePrettyJson"
+    "dzhibas/SublimePrettyJson",
+    "lonelyplanet/AtlasTestRunner"
     ]:
   }
 
