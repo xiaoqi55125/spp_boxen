2a0f86feaafe018d4bbd0dfb490da55c62db0c73
Matt Riddle <matthew.riddle@lonelyplanet.com.au>
2013-04-19 05:49:35 +1000

    Added support for my ssh keys and sublime license

--- modules/people/manifests/mriddle/config.pp
+++ modules/people/manifests/mriddle/config.pp
@@ -12,4 +12,25 @@ class people::mriddle::config {
     value => "log --pretty=format:'%C(yellow)%h%C(reset) %s %C(cyan)%cr%C(reset) %C(blue)%an%C(reset) %C(green)%d%C(reset)' --graph --date-order",
   }
 
+  file { "/Users/${::luser}/.id_rsa":
+    ensure  => link,
+    mode    => '0600',
+    target  => "${::boxen_srcdir}/private_dotfiles/ssh_private_key",
+    require => Repository["${::boxen_srcdir}/private_dotfiles"],
+  }
+
+  file { "/Users/${::luser}/.id_rsa.pub":
+    ensure  => link,
+    mode    => '0600',
+    target  => "${::boxen_srcdir}/private_dotfiles/ssh_public_key",
+    require => Repository["${::boxen_srcdir}/private_dotfiles"],
+  }
+
+  file { "/Users/${::luser}/Library/Application Support/Sublime Text 2/Settings/License.sublime_license":
+    ensure  => link,
+    mode    => '0640',
+    target  => "${::boxen_srcdir}/private_dotfiles/sublime_text.license",
+    require => Repository["${::boxen_srcdir}/private_dotfiles"],
+  }
+
 }
