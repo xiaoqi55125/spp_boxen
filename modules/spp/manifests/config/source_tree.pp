56605ca3caa0b27e1f7525c33029682139e0ef0d
David Hodges <david.hodges@lonelyplanet.com.au>
2013-04-18 13:36:47 +1000

    updated config for app: SourceTree

--- modules/spp/manifests/config/source_tree.pp
+++ modules/spp/manifests/config/source_tree.pp
@@ -1,17 +1,37 @@
 class config::source_tree {
 
-  $app_support_base = "/Users/${::luser}/Library/Application Support/SourceTree"
+  $app_support = "/Users/${::luser}/Library/Application Support/SourceTree"
 
-  file { $app_support_base:
+  file { $app_support:
     ensure  => 'directory',
     owner   => "${::luser}",
     mode    => '0755',
   }->
 
-  file { "${app_support_base}/sourcetree.license":
+  file { "${app_support}/sourcetree.license":
     ensure  => link,
     mode    => '0644',
     target  => "${::boxen_srcdir}/pairing_station/licenses/sourcetree.license",
     require => [Package['SourceTree'], Repository["${::boxen_srcdir}/pairing_station"]],
+  }
+
+  file { "${app_support}/browser.plist":
+    ensure  => present,
+    source  => "puppet:///modules/spp/source_tree/browser.plist"
+  }
+
+  file { "${app_support}/hgrc_sourcetree":
+    ensure  => present,
+    source  => "puppet:///modules/spp/source_tree/hgrc_sourcetree"
+  }
+
+  file { "${app_support}/hostingservices.plist":
+    ensure  => present,
+    source  => "puppet:///modules/spp/source_tree/hostingservices.plist"
+  }
+
+  file { "${app_support}/openWindowList":
+    ensure  => present,
+    source  => "puppet:///modules/spp/source_tree/openWindowList"
   }
 }
