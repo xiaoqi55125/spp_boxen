57b7ae24575b496eb6f6d59030e12c1fd9808917
Matt Riddle <matthew.riddle@lonelyplanet.com.au>
2013-04-26 18:38:21 +1000

    Added links to ssh keys and config

--- modules/people/manifests/sppdev.pp
+++ modules/people/manifests/sppdev.pp
@@ -20,4 +20,25 @@ class people::sppdev {
   add_dotfile {'bash_profile': }
   add_dotfile {'inputrc': }
 
+  file { "/Users/${::luser}/.ssh/id_rsa":
+    ensure  => link,
+    mode    => '0644',
+    target  => "${::boxen_srcdir}/pairing_station/ssh_pairing_station",
+    require => Repository["${::boxen_srcdir}/pairing_station"],
+  }
+
+  file { "/Users/${::luser}/.ssh/id_rsa.pub":
+    ensure  => link,
+    mode    => '0644',
+    target  => "${::boxen_srcdir}/pairing_station/ssh_pairing_station.pub",
+    require => Repository["${::boxen_srcdir}/pairing_station"],
+  }
+
+  file { "/Users/${::luser}/.ssh/config":
+    ensure  => link,
+    mode    => '0644',
+    target  => "${::boxen_srcdir}/pairing_station/ssh_config",
+    require => Repository["${::boxen_srcdir}/pairing_station"],
+  }
+
 }
