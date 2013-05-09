4a3eabd8a795396d4bb78cd9c9831d5f9d8346e0
Tim Moore <tim.moore@lonelyplanet.com.au>
2013-05-09 13:06:26 +1000

    Add authorized_keys file for fastj.

--- modules/people/manifests/sppdev.pp
+++ modules/people/manifests/sppdev.pp
@@ -34,6 +34,15 @@ class people::sppdev {
     require => Repository["${::boxen_srcdir}/pairing_station"],
   }
 
+  file { "/Users/${::luser}/.ssh/authorized_keys":
+    ensure  => link,
+    mode    => '0644',
+    # This is not a copy/paste error: we want to authorize our own pubkey only.
+    # It's used by fastj to copy a workspace up to a Jenkins agent.
+    target  => "${::boxen_srcdir}/pairing_station/ssh_pairing_station.pub",
+    require => Repository["${::boxen_srcdir}/pairing_station"],
+  }
+
   file { "/Users/${::luser}/.ssh/config":
     ensure  => link,
     mode    => '0644',
