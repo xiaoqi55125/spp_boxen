8bf40169fee3d67a17302984f7c1ce9f6d740278
Luke Work <lukew@Lukes-MacBook-Air.local>
2013-05-21 17:31:03 +1000

    Add lukerobins manifest

---
+++ modules/people/manifests/lukerobins.pp
@@ -0,0 +1,23 @@
+class people::lukerobins {
+
+  include spp::environment
+
+  repository { "${::boxen_srcdir}/pairing_station":
+    source => 'lonelyplanet/pairing_station',
+  }
+
+  define add_dotfile ($dotfile = $title) {
+    file { "/Users/${::luser}/.${dotfile}":
+      ensure  => link,
+      mode    => '0644',
+      target  => "${::boxen_srcdir}/pairing_station/${dotfile}",
+      require => Repository["${::boxen_srcdir}/pairing_station"],
+    }
+  }
+
+  add_dotfile {'bashrc': }
+  add_dotfile {'bash_aliases': }
+  add_dotfile {'bash_profile': }
+  add_dotfile {'inputrc': }
+
+}
