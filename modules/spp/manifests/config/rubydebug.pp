b49c3b3259cff8a47c9b28b347ce1e1d630cc158
David Hodges <david.hodges@lonelyplanet.com.au>
2013-07-11 10:52:40 +1000

    rdebugrc moved to repo: pairing_station

--- modules/spp/manifests/config/rubydebug.pp
+++ modules/spp/manifests/config/rubydebug.pp
@@ -2,8 +2,9 @@ class config::rubydebug {
 
   file { "/Users/${::luser}/.rdebugrc":
     ensure  => link,
-    target  => 'puppet:///modules/spp/rdebugrc',
+    target  => "${::boxen_srcdir}/pairing_station/rdebugrc",
     mode    => '0755',
+    require => Repository["${::boxen_srcdir}/pairing_station"],
   }
 
 }
