8178c9305c7c3cd2ea434fc8b5dab8ef7e559de2
Tim Moore <tim.moore@lonelyplanet.com.au>
2013-10-01 11:45:51 +1000

    Added download of dash files (not yet complete)

--- modules/spp/manifests/config/dash.pp
+++ modules/spp/manifests/config/dash.pp
@@ -3,19 +3,35 @@ class config::dash {
   $base = "/Users/${::luser}/Library/Application Support"
 
   $structure = [ "${base}/Dash" ]
+  $docSetsLink = "https://s3-ap-southeast-2.amazonaws.com/spp-boxen/DocSets.zip"
+  $dashFileLink = "https://s3-ap-southeast-2.amazonaws.com/spp-boxen/library.dash"
 
   file { $structure:
     ensure  => 'directory',
     owner   => "${::luser}",
     mode    => '0755',
-  }
+  }->
 
-  $docSetsLink = "https://s3-ap-southeast-2.amazonaws.com/spp-boxen/DocSets.zip"
+  exec { "curl -o library.dash ${dashFileLink}":
+    cwd      => "${base}/Dash",
+    provider => 'shell',
+    creates  => "${base}/Dash/library.dash",
+    path     => "/usr/bin",
+    require  => Package['Dash'],
+  }->
+
   exec { "curl -o DocSets.zip ${docSetsLink}":
     cwd      => "${base}/Dash",
     provider => 'shell',
     creates  => "${base}/Dash/DocSets.zip",
     path     => "/usr/bin",
     require  => Package['Dash'],
-  }
+  }->
+
+  exec { "unzip -o '${base}/Dash/DocSets.zip'":
+    cwd      => "${base}/Dash",
+    provider => 'shell',
+    path     => "/usr/bin",
+    require  => Package['Dash'],
+ }
 }
