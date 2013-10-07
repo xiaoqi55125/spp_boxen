7c50ffb01cf44c939cb1ca70f75776ab77f4e450
Jo Cranford <joanne.cranford@lonelyplanet.com.au>
2013-10-07 14:10:58 +1100

    Remove items from dock

--- modules/spp/manifests/config/dock.pp
+++ modules/spp/manifests/config/dock.pp
@@ -1,19 +1,5 @@
 class config::dock {
 
-  property_list_key { "Remove items kept in dock":
-    ensure => absent,
-    path   => "/Users/${::luser}/Library/Preferences/com.apple.dock.plist",
-    key    => 'persistent-apps',
-  }->
+  include osx::dock::clear_dock
 
-  property_list_key { "Remove link to documents/downloads":
-    ensure => absent,
-    path   => "/Users/${::luser}/Library/Preferences/com.apple.dock.plist",
-    key    => 'persistent-others'
-  }->
-
-  exec { "killall -HUP Dock":
-    provider => 'shell',
-    path     => "/usr/bin"
- }
 }
