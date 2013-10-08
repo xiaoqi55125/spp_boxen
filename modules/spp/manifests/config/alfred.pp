75afdf5ebca8724761d6b0a1c1092d533036a7a9
Jo Cranford <joanne.cranford@lonelyplanet.com.au>
2013-10-08 14:17:06 +1100

    Updating Alfred config to include license

--- modules/spp/manifests/config/alfred.pp
+++ modules/spp/manifests/config/alfred.pp
@@ -1,9 +1,21 @@
 class config::alfred {
 
-  file { "Alfred Preferences":
+  $licenseDir = "/Users/${::luser}/Library/Application Support/Alfred 2"
+  file { $licenseDir:
+    ensure  => 'directory',
+    owner   => "${::luser}",
+    mode    => '0755',
+  }
+
+  file { "Alfred License":
+    path    => "/Users/${::luser}/Library/Application Support/Alfred 2/license.plist",
+    source  => "puppet:///modules/spp/alfred.license.plist",
+    require => Package['Alfred'], File[$licenseDir],
+  }
+
+    file { "Alfred Preferences":
     path    => "/Users/${::luser}/Library/Preferences/com.runningwithcrayons.Alfred-Preferences.plist",
     source  => "puppet:///modules/spp/com.runningwithcrayons.Alfred-Preferences.plist",
     require => Package['Alfred'],
   }
-
 }
