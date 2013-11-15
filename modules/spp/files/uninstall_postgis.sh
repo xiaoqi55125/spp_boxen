2d024ee8cf864f4edbe3a0bef8b6db12c1261f0f
Tim Moore <tim.moore@lonelyplanet.com.au>
2013-11-15 12:00:46 +1100

    Handle ODB*
    * Ol' Dirty Boxen

--- modules/spp/files/uninstall_postgis.sh
+++ modules/spp/files/uninstall_postgis.sh
@@ -3,8 +3,26 @@
 echo "Stopping PostgreSQL..."
 launchctl unload /Library/LaunchDaemons/dev.postgresql.plist
 
+if [ -x /usr/bin/automake ]; then
+  echo "Removing old automake"
+  rm /usr/bin/automake*
+  rm /usr/bin/aclocal*
+fi
+
+if [ -x /usr/bin/autoconf ]; then
+  echo "Removing old autoconf"
+  rm /usr/bin/autoconf*
+  rm /usr/bin/autoheader*
+  rm /usr/bin/autom4te*
+  rm /usr/bin/autoreconf*
+  rm /usr/bin/autoscan*
+  rm /usr/bin/autoupdate*
+  rm /usr/bin/ifnames*
+fi
+
 echo "Uninstalling PostGIS and all of its dependencies..."
-brew rm postgis \
+brew rm --force \
+        postgis \
           automake \
             autoconf \
           gpp \
